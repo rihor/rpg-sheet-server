cbc618f1d93ea5d7fa58072b21b939d83a6f5be1
Pedro Pinho <rihor1000@gmail.com>
2020-12-19 11:12:31 -0300

    Moving orm config to a js file & setting up babel

---
+++ ormconfig.js
@@ -0,0 +1,31 @@
+const isProduction = process.env.NODE_ENV === "production"
+const rootFolder = isProduction ? "build" : "src"
+
+const entitiesFolder = `./${rootFolder}/modules/**/infra/typeorm/entities/*.ts`
+
+console.log("Is looking for entities at: ", entitiesFolder)
+
+module.exports = [
+  {
+    "name": "default",
+    "type": "postgres",
+    "host": process.env.PG_HOST,
+    "port": process.env.PG_PORT,
+    "username": process.env.PG_USERNAME,
+    "password": process.env.PG_PASSWORD,
+    "database": process.env.PG_DATABASE,
+    "entities": [entitiesFolder],
+    "migrations": [`./${rootFolder}/shared/infra/typeorm/migrations/*.ts`],
+    "cli": {
+      "migrationsDir": `./${rootFolder}/shared/infra/typeorm/migrations/`
+    }  },
+  {
+    "name": "mongo",
+    "type": "mongodb",
+    "host": process.env.MONGO_HOST,
+    "port": process.env.MONGO_PORT,
+    "database": process.env.MONGO_DATABASE,
+    "useUnifiedTopology": true,
+    "entities": [`./${rootFolder}/modules/**/infra/typeorm/schemas/*.ts`]
+  }
+]
--- src/@types/index.d.ts
+++ src/@types/index.d.ts
@@ -4,5 +4,15 @@ declare namespace NodeJS {
     NODE_ENV: string
     APP_API_URL: string
     PORT: string
+
+    PG_DATABASE: string
+    PG_HOST: string
+    PG_PORT: number
+    PG_USERNAME: string
+    PG_PASSWORD: string
+
+    MONGO_HOST: string
+    MONGO_PORT: number
+    MONGO_DATABASE: string
   }
 }
