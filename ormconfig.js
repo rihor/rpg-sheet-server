ebbc662411a57280a9981075b72b0da77853f724
Pedro Pinho <rihor1000@gmail.com>
2021-01-04 09:42:02 -0300

    Adding mongo auths

--- ormconfig.js
+++ ormconfig.js
@@ -24,6 +24,8 @@ module.exports = [
     "type": "mongodb",
     "host": process.env.MONGO_HOST,
     "port": process.env.MONGO_PORT,
+    "username": process.env.MONGO_USERNAME,
+    "password": process.env.MONGO_PASSWORD,
     "database": process.env.MONGO_DATABASE,
     "useUnifiedTopology": true,
     "entities": [`./${rootFolder}/modules/**/infra/typeorm/schemas/*.ts`]
