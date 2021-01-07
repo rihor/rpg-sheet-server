85a5a5f9d3a08db8906a0a6348c7fcf2fe32d0a1
Pedro Pinho <rihor1000@gmail.com>
2021-01-07 13:24:13 -0300

    Changing mongo envs to a single url

--- ormconfig.js
+++ ormconfig.js
@@ -19,19 +19,22 @@ module.exports = [
     cli: {
       migrationsDir: `./${rootFolder}/shared/infra/typeorm/migrations/`,
     },
-    ssl: {
-      rejectUnauthorized: false,
-    },
+    ...(isProduction && {
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    }),
+    // ssl: {
+    //   rejectUnauthorized: !isProduction,
+    // },
   },
   {
     name: "mongo",
     type: "mongodb",
-    host: process.env.MONGO_HOST,
-    port: process.env.MONGO_PORT,
-    username: process.env.MONGO_USERNAME,
-    password: process.env.MONGO_PASSWORD,
-    database: process.env.MONGO_DATABASE,
+    url: process.env.MONGO_URL,
     useUnifiedTopology: true,
+    useNewUrlParser: true,
+    synchronize: true,
     entities: [`./${rootFolder}/modules/**/infra/typeorm/schemas/*.ts`],
   },
 ]
