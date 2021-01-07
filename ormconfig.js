2e42217ab31c517a75e4d925a22674b3e3ce54cf
Pedro Pinho <rihor1000@gmail.com>
2021-01-07 12:17:53 -0300

    SSL off fix

--- ormconfig.js
+++ ormconfig.js
@@ -7,27 +7,31 @@ console.log("Is looking for entities at: ", entitiesFolder)
 
 module.exports = [
   {
-    "name": "default",
-    "type": "postgres",
-    "host": process.env.PG_HOST,
-    "port": process.env.PG_PORT,
-    "username": process.env.PG_USERNAME,
-    "password": process.env.PG_PASSWORD,
-    "database": process.env.PG_DATABASE,
-    "entities": [entitiesFolder],
-    "migrations": [`./${rootFolder}/shared/infra/typeorm/migrations/*.ts`],
-    "cli": {
-      "migrationsDir": `./${rootFolder}/shared/infra/typeorm/migrations/`
-    }  },
+    name: "default",
+    type: "postgres",
+    host: process.env.PG_HOST,
+    port: process.env.PG_PORT,
+    username: process.env.PG_USERNAME,
+    password: process.env.PG_PASSWORD,
+    database: process.env.PG_DATABASE,
+    entities: [entitiesFolder],
+    migrations: [`./${rootFolder}/shared/infra/typeorm/migrations/*.ts`],
+    cli: {
+      migrationsDir: `./${rootFolder}/shared/infra/typeorm/migrations/`,
+    },
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  },
   {
-    "name": "mongo",
-    "type": "mongodb",
-    "host": process.env.MONGO_HOST,
-    "port": process.env.MONGO_PORT,
-    "username": process.env.MONGO_USERNAME,
-    "password": process.env.MONGO_PASSWORD,
-    "database": process.env.MONGO_DATABASE,
-    "useUnifiedTopology": true,
-    "entities": [`./${rootFolder}/modules/**/infra/typeorm/schemas/*.ts`]
-  }
+    name: "mongo",
+    type: "mongodb",
+    host: process.env.MONGO_HOST,
+    port: process.env.MONGO_PORT,
+    username: process.env.MONGO_USERNAME,
+    password: process.env.MONGO_PASSWORD,
+    database: process.env.MONGO_DATABASE,
+    useUnifiedTopology: true,
+    entities: [`./${rootFolder}/modules/**/infra/typeorm/schemas/*.ts`],
+  },
 ]
